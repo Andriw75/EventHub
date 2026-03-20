3adc3f3e4bd843aaf2d87b2f7b5becbfec7aaea9
Andriw75 <andriw7522@gmail.com>
2026-03-20 16:37:49 -0500

    mejoramos el esquema para manejar logica de usuarios

--- backend/infrastructure/_db/schem.sql
+++ backend/infrastructure/_db/schem.sql
@@ -2,17 +2,15 @@ ALTER DATABASE railway SET timezone TO 'America/Lima';
 
 CREATE TABLE "User" (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(250) NOT NULL UNIQUE,
-    password VARCHAR(250) NOT NULL,
-    permissions TEXT [] NOT NULL DEFAULT '{}'
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password CHAR(60) NOT NULL,
+    correo VARCHAR(254) UNIQUE,
+    estado BOOLEAN DEFAULT FALSE,
+    codigo INTEGER NULL,
+    permissions TEXT[] NOT NULL DEFAULT '{}',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO "User" (username, password)
-VALUES (
-    'andriwdv',
-    '$2a$12$JMs1p1czE2U1zwmHLtJjvuv4SCLmcNkOa6gejOlKP2Yo3ejLPZpeC'
-);
-
-
-
-
+INSERT INTO "User" (username,password,correo,estado)
+VALUES ('ING. AndriwDV','$2a$12$Vr.gWaUTcXh.1JZ8OhNmQux1qjzyA6gOML1hEmJMLWLOLhkpZyi9a','andriw7522@gmail.com',TRUE);

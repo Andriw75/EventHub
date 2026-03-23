fdbf7038173085cf605aa6e21a13ef80a60edc24
Andriw75 <andriw7522@gmail.com>
2026-03-23 10:18:52 -0500

    modificamos para dejarlo mas legible y agregamos register

--- frontend/src/infrastructure/auth.ts
+++ frontend/src/infrastructure/auth.ts
@@ -1,10 +1,10 @@
 import type { User } from "../domain/auth";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const AUTH_URL = `${import.meta.env.VITE_API_URL}/auth`;
 
 export const authService = {
   async fetchMe(): Promise<User | null> {
-    const res = await fetch(`${API_URL}/auth/me`, {
+    const res = await fetch(`${AUTH_URL}/me`, {
       credentials: "include",
     });
 
@@ -17,16 +17,19 @@ export const authService = {
       formData.append("username", username);
       formData.append("password", password);
 
-      const res = await fetch(`${API_URL}/auth/token`, {
+      const res = await fetch(`${AUTH_URL}/token`, {
         method: "POST",
         body: formData,
         credentials: "include",
       });
+
       const data = await res.json();
+
       if (!res.ok) {
         console.error("Error del backend:", data);
         throw new Error(data?.detail || "Error en login");
       }
+
       return data;
     } catch (err) {
       console.error("Error en login:", err);
@@ -34,8 +37,39 @@ export const authService = {
     }
   },
 
+  async register(
+    correo: string,
+    name: string,
+    password: string,
+  ): Promise<boolean> {
+    try {
+      const res = await fetch(`${AUTH_URL}/create-user`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          correo,
+          name,
+          password,
+        }),
+      });
+
+      if (!res.ok) {
+        const data = await res.json();
+        console.error("Error del backend:", data);
+        return false;
+      }
+
+      return true;
+    } catch (err) {
+      console.error("Error en register:", err);
+      return false;
+    }
+  },
+
   async logout(): Promise<void> {
-    await fetch(`${API_URL}/auth/logout`, {
+    await fetch(`${AUTH_URL}/logout`, {
       method: "POST",
       credentials: "include",
     });

fda2469fe700108469be029396dc6a86c8e242f5
Andriw75 <andriw7522@gmail.com>
2026-03-24 09:48:29 -0500

    implementamos redireccion en el dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Navigate } from "@solidjs/router";
 import Login from "./app/Pages/Login/Login";
 import CreateAccount from "./app/Pages/CreateAccount/CreateAccount";
 import PersonHome from "./app/Pages/Person/PersonHome";
+import Dashboard from "./app/Pages/Dashboard/Dashboard";
 
 export default function App() {
   return (
@@ -11,7 +12,7 @@ export default function App() {
 
       <Route path="/:person" component={() => <PersonHome />} />
 
-      <Route path="/:person/dashboard" component={() => <>Dashboard</>} />
+      <Route path="/:person/dashboard" component={() => <Dashboard />} />
       <Route path="/:person/:event" component={() => <>EventDetails</>} />
 
       <Route path="/" component={() => <Navigate href="/login" />} />
